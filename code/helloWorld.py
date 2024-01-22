d492f08e80620aa21b2490dc7fb4229c42b1e688
wpgoble <will.goble@gmail.com>
2024-01-22 14:57:22 -0500

    adds code and slides from day 1

---
+++ code/helloWorld.py
@@ -0,0 +1,10 @@
+# below we have two examples for how we can print to the screen
+print("Hello World")    # Here our string uses double quotes
+print('Go Birds')       # Here our string uses single quotes
+
+# Below we will ask the user for their favorite color and
+# store it in the variable favoriteColor. Then we can print
+# the variable favoriteColor to the screen.
+favoriteColor = input("What's your favorite color? ")
+print("Your favorite color is", favoriteColor)
+
