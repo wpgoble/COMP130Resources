8ecbb30a8dc9c7e8fb7f57a5b30bf98581102189
wpgoble <will.goble@gmail.com>
2024-01-25 15:51:45 -0500

    code from the second day of class

---
+++ code/day2.py
@@ -0,0 +1,15 @@
+print(type(42))
+
+pi = 3.1415
+print(pi)
+pi = 1123.234
+print(pi)
+
+# values with special chars
+email = "oqibz@example.com"
+excitement = "Yay!"
+big_number = 1,000,000
+print(big_number)
+bigger_number = 1_000_000_000
+print(bigger_number)
+print(1/3)
