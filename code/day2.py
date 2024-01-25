8264da1bf6ee9abc04854625bfa003163fd630e7
wpgoble <will.goble@gmail.com>
2024-01-25 15:54:04 -0500

    adds comments to the initial day 2 code

--- code/day2.py
+++ code/day2.py
@@ -1,7 +1,11 @@
+# Let's see what the type function does
 print(type(42))
 
+# Here we use an assignment statement to 
+# Create the variable pi and assign it an initial Value
 pi = 3.1415
 print(pi)
+# What happens if we reassign it?
 pi = 1123.234
 print(pi)
 
@@ -10,6 +14,9 @@ email = "oqibz@example.com"
 excitement = "Yay!"
 big_number = 1,000,000
 print(big_number)
+# To make our code easier for us to read, if 
+# we have large numbers then we can use the underscore
+# to separate the degrees of magnitude 
 bigger_number = 1_000_000_000
 print(bigger_number)
 print(1/3)
